e279a933128b9e36a57ee5680376f5ee80e137dd
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2021-02-13 22:28:27 +0100

    Ready. Set. Go!

---
+++ public/front.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+
+$request = Request::createFromGlobals();
+$routes = include __DIR__ . "/../src/app.php";
+
+$context = new RequestContext();
+$context->fromRequest($request);
+$matcher = new UrlMatcher($routes, $context);
+
+function render_template(Request $request) {
+	extract($request->attributes->all(), EXTR_SKIP);
+
+	ob_start();
+	include sprintf(__DIR__ . "/../src/pages/%s.php", $_route);
+
+	return new Response(ob_get_clean());
+}
+
+try {
+	$request->attributes->add($matcher->match($request->getPathInfo()));
+	$response = call_user_func($request->attributes->get("_controller"), $request);
+} catch(ResourceNotFoundException $exception) {
+	$response = new Response("Not Found", 404);
+} catch(Exception $exception) {
+	$response = new Response("Internal Server Error", 500);
+}
+
+$response->send();
---
+++ src/app.php
@@ -0,0 +1,38 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+
+function is_leap_year($year = null) {
+	if (null === $year) {
+		$year = date("Y");
+	}
+
+	return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
+}
+
+$routes = new RouteCollection();
+
+$routes->add('hello', new Route('/hello/{name}', [
+	'name' => 'World',
+	"_controller" => function (Request $request) {
+		$request->attributes->set("foo", "bar");
+		return render_template($request);
+	}
+]));
+$routes->add('bye', new Route('/bye'));
+
+$routes->add("leap_year", new Route("/is_leap_year/{year}", [
+	"year" => null,
+	"_controller" => function (Request $request) {
+		if (is_leap_year($request->attributes->get("year"))) {
+			return new Response("Yap, this is a leap year");
+		}
+
+		return new Response("Nope, this is not a leap year.");
+	}
+]));
+
+return $routes;
